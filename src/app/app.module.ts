1025293a6967ac8c681a459a80d504613f822439
GersonRamos001 <ramosargueta@gmail.com>
2023-09-23 20:17:32 -0600

    Added Component Calculadora-Salario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculadoraSalarioComponent } from './calculadora-salario/calculadora-salario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculadoraSalarioComponent
   ],
   imports: [
     BrowserModule
