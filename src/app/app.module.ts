533c5e632083c349bd4e3fb637045fa9c813f72a
GersonRamos001 <ramosargueta@gmail.com>
2023-09-23 20:36:18 -0600

    added some functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { CalculadoraSalarioComponent } from './calculadora-salario/calculadora-salario.component';
+import { CalculadoraSalarioComponent } from './calculadora-salario/calculadora-salario.component'; // Asegúrate de importar el componente
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalculadoraSalarioComponent
+    CalculadoraSalarioComponent // Añade el componente a las declaraciones
   ],
   imports: [
-    BrowserModule
+    RouterModule,
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/calculadora-salario/calculadora-salario.component.ts
+++ src/app/calculadora-salario/calculadora-salario.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculadora-salario.component.css']
 })
 export class CalculadoraSalarioComponent {
+  salario: number | undefined;
+  descuentoRenta: number | undefined;
+  descuentoAFP: number | undefined;
+  descuentoISSS: number | undefined;
+  salarioLiquido: number | undefined;
 
+  calcularDescuentoRenta() {
+    if (this.salario !== undefined) {
+      if (this.salario >= 0.01 && this.salario <= 472.00) {
+        this.descuentoRenta = 0;
+      } else if (this.salario >= 472.01 && this.salario <= 895.24) {
+        this.descuentoRenta = (((this.salario - ((this.salario * 0.0725) + (this.salario * 0.03))) - 472) * 0.1 + 17.67);
+      } else if (this.salario >= 895.25 && this.salario <= 2038.10) {
+        this.descuentoRenta = (((this.salario - ((this.salario * 0.0725) + (this.salario * 0.03))) - 895.24) * 0.2 + 60.00);
+      } else if (this.salario >= 2038.11) {
+        this.descuentoRenta = (((this.salario - ((this.salario * 0.0725) + (this.salario * 0.03))) - 2038.10) * 0.3 + 288.57);
+      }
+    }
+  }
+
+  calcularSalarioLiquido() {
+    if (this.salario !== undefined) {
+      this.calcularDescuentoRenta();
+      this.descuentoAFP = this.salario * 0.0725;
+      this.descuentoISSS = this.salario * 0.03;
+      this.salarioLiquido = this.salario - this.descuentoAFP - this.descuentoISSS - (this.descuentoRenta || 0);
+    }
+  }
 }
+
